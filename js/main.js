ab61638f828d1a2e129b4a367cc11e1b8dab7586
Marko Jordanovic <mrakaconi@gmail.com>
2019-02-06 17:09:05 +0100

    promena koda kompletno

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
-// Izabrati 35 random brojeva od 48 brojeva
-
-var niz = [];
-function addNumbers(niz) {
-    for (i = 0; i < 35; i++) {
-        var dodajBroj = true;
+function createNiz35() {
+    var niz35 = [];
+    i = 0;
+    do {
         var rng = Math.floor(Math.random() * 48) + 1;
-        for (var y = 0; y <= 48; y++) {
-            if (niz[y] == rng) {
-                dodajBroj = false;
-            }
-        }
-        // postaviti dobijene brojeve u niz
-        if (dodajBroj) {
-            niz.push(rng)
-        } else {
-            i--;
+        if (!niz35.includes(rng)) {
+            niz35.push(rng);
+            i++;
         }
     }
+    while (i < 35)
+    return niz35
+}
+createNiz35();
+
+function multiplyNiz(total_number) {
+    var nizNizova = [];
+    for (z = 0; z <= total_number; z++) {
+        var niz35 = createNiz35();
+
+        nizNizova.push(niz35)
+    }
+    return nizNizova;
+}
+var ukupno_nizova = 3;
+console.log(multiplyNiz(ukupno_nizova));
+
+
+/*
+ponavljanje[1] = 0 
+ponavljanje[2] = 0 
+ponavljanje[3] = 0 
+...
+ponavljanje[22] = 0 
+...
+ponavljanje[48] = 0 
+
+
+niz_od_35 = multiplyNiz(ukupno_nizova)[0]
+
+niz_od_35 = [ 22, 31, 28, 5, 35, 9, 30, 15, 23, 25, … ]
+[ 35, 47, 19, … ]
+[ 24, 38, 35, … ]
+
+for (i = 0; i < 35; i++) {
+	jedan_clan_niza = niz_od_35[i];
+	ponavljanje[jedan_clan_niza] = ponavljanje[jedan_clan_niza] + 1;
 }
 
-//Miloske, uspeo sam da postavim sa ovim delom svih 300 iteracija u NIZ ali su mi svi brojevi u svih 300 nizova isti. Muci me kako da namestim da mi za svaki niz vadi jovo na novo random brojeve.
-// Ajde ako ti nije mrsko, proveri kod pa vidi gde mi sta fali ... Hvala !!!
-addNumbers(niz);
-var noviNiz = [];
-for (z = 0; z <= 2; z++)
-    noviNiz.push(niz);
-console.log(noviNiz);
-
-
-
+[1] = 17
+[2] = 17
+[3] = 17
+[4] = 0
+[5] = 12
+[6] = 6
+...
+[19] = 3
 
 
 
@@ -36,3 +62,27 @@ console.log(noviNiz);
 // proveriti niz kojih se sest (6) brojeva najcesce (high drop chance) pojavljuju i postaviti ih u novi niz (high) i prikazati koliko se puta ponavljaju u 300 iteracija
 // proveriti niz kojih se sest (6) brojeva redje (medium drop chance) pojavljuju i postaviti ih u novi niz (medium) i prikazati koliko se puta ponavljaju u 300 iteracija
 // proveriti niz kojih se sest (6) brojeva najredje (low drop chance)pojavljuju i postaviti ih u novi niz (rare) i prikazati koliko se puta ponavljaju u 300 iteracija
+ 1 - 20
+ 21 - 40
+ 41 -
+ 
+ 1
+ 
+ 22
+
+var numbers = [1, 2, 3, 4];
+Math.max.apply(null, numbers) // 4
+Math.min.apply(null, numbers) // 1
+
+
+ 
+
+
+
+
+
+var ponavljanje = [];
+
+ponavljanje[22] = ponavljanje[22]++;
+
+*/
